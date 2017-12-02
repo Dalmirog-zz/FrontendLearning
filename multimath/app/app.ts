f3a2d5495006054078c6a1e93e1b870b644e1b05
dalmirog <dalmiro.granias@Gmail.com>
2017-12-02 12:27:05 -0300

    End of "Taking Advantage of Built-in Types"

--- multimath/app/app.ts
+++ multimath/app/app.ts
@@ -1,7 +1,17 @@
 function startGame(){
     // Starting a new game
-    var messagesElement = document.getElementById('messages');
-    messagesElement.innerText = 'Welcome to MultiMath! Starting a new game...';
-    console.log('Starting new game!.');
+
+    let playerName: string = 'Audrey';    
+    logPlayer(playerName);
+
+    let messagesElement: HTMLElement | null = document.getElementById('messages');
+
+    messagesElement!.innerText = 'Welcome to MultiMath! Starting a new game...!';
+    console.log('Starting new game!');
 }
-document.getElementById('startGame').addEventListener('click',startGame);
+
+function logPlayer(name){
+    console.log(`New game starting for player: ${name}`);
+}
+
+document.getElementById('startGame')!.addEventListener('click',startGame);
