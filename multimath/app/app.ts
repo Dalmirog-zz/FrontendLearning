48ca8a03e267db36c9c34bf2e782886406f4fe02
dalmirog <dalmiro.granias@Gmail.com>
2017-12-05 13:21:15 -0300

    End of "Writing Better Functions with TypeScript"

--- multimath/app/app.ts
+++ multimath/app/app.ts
@@ -1,17 +1,48 @@
 function startGame(){
     // Starting a new game
 
-    let playerName: string = 'Audrey';    
+    let playerName: string | undefined = getInputValue('playername');    
     logPlayer(playerName);
 
-    let messagesElement: HTMLElement | null = document.getElementById('messages');
-
-    messagesElement!.innerText = 'Welcome to MultiMath! Starting a new game...!';
-    console.log('Starting new game!');
+    postSCore(80, playerName);
+    postSCore(-5, playerName);
 }
-
-function logPlayer(name){
+function logPlayer(name: string = ' MultiMath Player '): void {
     console.log(`New game starting for player: ${name}`);
 }
 
+function postSCore(score: number, playerName: string = 'MultiMath Player'): void{
+
+    let logger: (value: string) => void;
+    
+    if(score < 0){
+        logger = logError;
+    }
+    else{
+        logger = logMessage;
+    }
+
+    let scoreElement: HTMLElement | null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
+
+    logger(`Score: ${score}`)
+
+}
+
+function getInputValue(elementID: string): string | undefined {
+    let inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID)
+
+    if  (inputElement.value === ''){
+        return undefined;
+    }
+    else{
+        return inputElement.value;
+    }
+}
 document.getElementById('startGame')!.addEventListener('click',startGame);
+
+let logMessage = (message: string) => console.log(message);
+
+function logError(error: string): void {
+    console.error(error);
+}
